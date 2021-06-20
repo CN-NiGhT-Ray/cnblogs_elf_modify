d30704205f9dbf4431108ea68bb36d886d6f231a
CN_NiGhT_Ray <50533665+CN-NiGhT-Ray@users.noreply.github.com>
2021-06-20 09:21:59 +0800

    Create elf_mod.js

---
+++ dist/elf_mod.js
@@ -0,0 +1,18 @@
+<div class="head_img"><img width="120" height="120" alt="我的头像" src="//pic.cnblogs.com/avatar/1014286/20160824172911.png" class="img_avatar"></div>
+<script charset="utf-8" src="https://cdn.jsdelivr.net/npm/live2d-widget@3.1.4/lib/L2Dwidget.min.js"></script>
+<script>
+    L2Dwidget.init({
+        "model": {
+            jsonPath: "https://cdn.jsdelivr.net/gh/cn-night-ray/live2d-widget-models@1.0/packages/live2d-widget-model-shizuku/assets/shizuku.model.json",
+            scale: 1
+        },
+        "display": {
+            "superSample": 1.2,
+            "width": 200,
+            "height": 380,
+            "position": "right",
+            "hOffset": 0,
+            "vOffset": -32
+        }
+    });
+</script>
