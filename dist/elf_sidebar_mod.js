1b26bb1591abad77054c5acd1d846a1a8998fe65
CN_NiGhT_Ray <50533665+CN-NiGhT-Ray@users.noreply.github.com>
2021-06-20 09:31:02 +0800

    Update and rename elf_mod.js to elf_sidebar_mod.js

--- dist/elf_sidebar_mod.js
+++ dist/elf_sidebar_mod.js
@@ -1,5 +1,10 @@
+// 博客侧边栏公告（支持HTML代码）
+
+// 添加了个人头像
 <div class="head_img"><img width="120" height="120" alt="我的头像" src="//pic.cnblogs.com/avatar/1014286/20160824172911.png" class="img_avatar"></div>
+// 添加了看板娘
 <script charset="utf-8" src="https://cdn.jsdelivr.net/npm/live2d-widget@3.1.4/lib/L2Dwidget.min.js"></script>
+// 看板娘配置参数
 <script>
     L2Dwidget.init({
         "model": {
